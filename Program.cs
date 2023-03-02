22a33f2dc1ae419036dad904892c862e9d9c598b
Henry Ison (PC) <hison259@gmail.com>
2023-03-01 23:53:16 -0600

    more stuff

--- Program.cs
+++ Program.cs
@@ -8,26 +8,74 @@ namespace EvolutionSimulator
 {
     class Creature
     {
+        private Habitat habitat;
         private int gene;
         private int xpos;
         private int ypos;
-        public Creature(int xpos, int ypos)
+        public Creature(Habitat habitat, int xpos, int ypos)
         {
+            this.habitat = habitat;
             this.xpos = xpos;
             this.ypos = ypos;
             this.gene = 0;
         }
 
-        public Creature(int xpos, int ypos, int gene)
+        public Creature(Habitat habitat, int xpos, int ypos, int gene)
         {
+            this.habitat = habitat;
             this.xpos = xpos;
             this.ypos = ypos;
             this.gene = gene;
         }
 
-        public int getGene()
+        public int Gene => this.gene;
+
+        private float SenseUp()
         {
-            return this.gene;
+            if (habitat.GetPosition(xpos, ypos-1) == null)
+            {
+                return 0;
+            }
+            else
+            { 
+                return 1; 
+            }
+        }
+
+        private float SenseDown()
+        {
+            if (habitat.GetPosition(xpos, ypos + 1) == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private float SenseRight()
+        {
+            if (habitat.GetPosition(xpos + 1, ypos) == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        private float SenseLeft()
+        {
+            if (habitat.GetPosition(xpos - 1, ypos) == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
         }
 
         public void Action()
@@ -36,7 +84,7 @@ namespace EvolutionSimulator
         }
     }
 
-    abstract class Environment
+    abstract class Habitat
     {
         private List<Creature> creatures = new List<Creature>();
         private int generation;
@@ -47,13 +95,14 @@ namespace EvolutionSimulator
         private int maxChild;
         private int length;
         private int width;
+        private int iModifier;
         private int modifier;
-        private Creature[][] positions;
+        private Creature[,] positions;
 
 
-        public Environment(int length, int width, int maxTime, int creatureAmount, int maxChild, int reproduceChance, int mutationChance, int modifier)
+        public Habitat(int length, int width, int maxTime, int creatureAmount, int maxChild, int reproduceChance, int mutationChance, int modifier)
         {
-            this.generation = 0;
+            generation = 0;
             this.maxTime = maxTime;
             this.mutationChance = mutationChance;
             this.reproduceChance = reproduceChance;
@@ -68,102 +117,113 @@ namespace EvolutionSimulator
             this.maxChild = maxChild;
             this.length = length;
             this.width = width;
-            this.positions = new Creature[length][width];
+            iModifier = modifier;
+            this.modifier = modifier;
+            positions = new Creature[length, width];
             Random randX;
             Random randY;
             int xpos;
             int ypos;
             for (int i = 0; i < creatureAmount; i++) {
                 randX = new Random(i+modifier);
-                randY = new Random((i+modifier)*2)
+                randY = new Random((i + modifier) * 2);
                 xpos = randX.Next(0, width);
                 ypos = randY.Next(0, length);
-                while (positions[ypos][xpos] != null) {
+                while (positions[ypos,xpos] != null) {
                     xpos += 1;
                     ypos += 1;
                     if (xpos >= width) { xpos = 0; }
                     if (ypos >= length) { ypos = 0; }
                 }
-                creatures.Add(new Creature(xpos, ypos));
-                positions[ypos][xpos] = creatures[i];
+                creatures.Add(new Creature(this, xpos, ypos));
+                positions[ypos,xpos] = creatures[i];
             }
         }
 
-        public bool survive(Creature creature)
+        private int Modify()
+        {
+            Random rand = new Random(modifier);
+            modifier = rand.Next(iModifier);
+            return modifier;
+        }
+
+        public bool Survive(Creature creature)
         {
             return true;
         }
 
         public Creature GetCreature(int index)
         {
-            return this.creatures[index];
+            return creatures[index];
         }
 
         public Creature GetPosition(int x, int y)
         {
-            return this.positions[y][x];
+            return positions[y,x];
         }
 
-        public int getGeneration()
+        public int GetGeneration()
         {
-            return this.generation;
+            return generation;
         }
 
-        private int mutate(int gene) {
+        private int Mutate(int gene) {
             int newGene = gene;
             return newGene;
         }
 
-        private int reproduce(List<Creature> parents) 
+        private void Reproduce(List<Creature> parents) 
         {
-              this.creatures = new List<Creature>();
-              Random randX;
-              Random randY;
-              Random rand;
-              int xpos;
-              int ypos;
-              for (int i = 0; i < this.maxChild*parents.Count; i++) {
-                  rand = new Random(i+this.generation+this.modifier);
-                  if (rand.Next(0, 100) < this.reproduceChance && this.creatures.Count < this.creatureAmount)
-                  {
-                      randX = new Random((i+this.generation+this.modifier)*2);
-                      randY = new Random((i+this.generation+this.modifier)*3)
-                      xpos = randX.Next(0, this.width);
-                      ypos = randY.Next(0, this.length);
-                      while (this.positions[ypos][xpos] != null) {
-                          xpos += 1;
-                          ypos += 1;
-                          if (xpos >= this.width) { xpos = 0; }
-                          if (ypos >= this.length) { ypos = 0; }
-                      }
-                      this.creatures.Add(new Creature(xpos, ypos, mutate(parents[i%parents.Count].getGene()));
-                      this.positions[ypos][xpos] = this.creatures[i];
-                  }
-              }
+            creatures = new List<Creature>();
+            positions = new Creature[length, width];
+            Random randX;
+            Random randY;
+            Random rand;
+            int xpos;
+            int ypos;
+            for (int i = 0; i < maxChild *parents.Count; i++) {
+                rand = new Random(i + generation + Modify());
+                if (rand.Next(0, 100) < reproduceChance && creatures.Count < creatureAmount)
+                {
+                    randX = new Random(i + generation + Modify());
+                    randY = new Random(i + generation + Modify());
+                   xpos = randX.Next(0, width);
+                   ypos = randY.Next(0, length);
+                   while (positions[ypos,xpos] != null) 
+                   {
+                       xpos += 1;
+                       ypos += 1;
+                       if (xpos >= width) { xpos = 0; }
+                       if (ypos >= length) { ypos = 0; }
+                   }
+                   creatures.Add(new Creature(this, xpos, ypos, Mutate(parents[i%parents.Count].Gene)));
+                   positions[ypos,xpos] = creatures[i];
+                }
+            }
         }
 
-        public void lifeCycle()
+        public void LifeCycle()
         {
-            for (int i = 0; i < this.maxTime; i++)
+            for (int i = 0; i < maxTime; i++)
             {
-                for (int j = 0; j < this.creatures.Count; j++)
+                for (int j = 0; j < creatures.Count; j++)
                 {
-                    this.creatures[j].Action();
+                    creatures[j].Action();
                 }
             }
 
-            for (int i = 0; i < this.creatures.Count; i++) 
+            for (int i = 0; i < creatures.Count; i++) 
             {
-                if (!survive(this.creatures[i])) 
+                if (!Survive(creatures[i])) 
                 {
-                  this.creatures.RemoveAt(i);
-                  i--;
+                    creatures.RemoveAt(i);
+                    i--;
                 }
             }
 
-            List<Creature> parents = this.creatures;
-            reproduce(parents);
-
+            List<Creature> parents = creatures;
+            Reproduce(parents);
+            
             
         }
     }
