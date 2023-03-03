142b491689c8adf556a96def16e4bd7c472481a9
Henry Ison (PC) <hison259@gmail.com>
2023-03-02 23:35:15 -0600

    Added genes

--- Program.cs
+++ Program.cs
@@ -6,29 +6,121 @@ using System.Threading.Tasks;
 
 namespace EvolutionSimulator
 {
+    class Gene
+    {
+        private int startType;
+        private int endType;
+        private int startID;
+        private int endID;
+        private int weight;
+        private int encoding;
+
+        public Gene(int num, bool encoded)
+        {
+            if (encoded)
+            {
+                this.encoding = num;
+            }
+            else
+            {
+                Random rand = new Random(num);
+                this.encoding = rand.Next(0, 268435455);
+            }
+
+            int[] bits = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+            for (int i = 0; i < 7; i++)
+            {
+                int digit = (int)(encoding % 0x10);
+                if (digit >= 8)
+                {
+                    bits[i * 4 + 3] = 1;
+                    digit -= 8;
+                }
+                if (digit >= 4)
+                {
+                    bits[i * 4 + 2] = 1;
+                    digit -= 4;
+                }
+                if (digit >= 2)
+                {
+                    bits[i * 4 + 1] = 1;
+                    digit -= 2;
+                }
+                if (digit >= 1)
+                {
+                    bits[i * 4] = 1;
+                }
+                encoding /= 0x10;
+            }
+
+            this.startType = bits[27];
+            this.endType = bits[19];
+
+            int mult = 1;
+            this.startID = 0;
+            this.endID = 0;
+            for (int i = 12; i < 19; i++)
+            {
+                this.startID += bits[i + 8] * mult;
+                this.endID += bits[i] * mult;
+                mult *= 2;
+            }
+
+            mult = 1;
+            this.weight = 0;
+            for (int i = 0; i < 12; i++)
+            {
+                this.weight += bits[i] * mult;
+                mult *= 2;
+            }
+        }
+        
+        
+
+        public int StartType => this.startType;
+
+        public int EndType => this.endType;
+
+        public int StartID => this.startID;
+
+        public int EndID => this.endID;
+
+        public int Weight => this.weight;
+
+    }
+    
     class Creature
     {
         private Habitat habitat;
-        private int gene;
+        private int[] genes;
+        private int[] neurons = new int[3];
+        private int connections = 8;
         private int xpos;
         private int ypos;
-        public Creature(Habitat habitat, int xpos, int ypos)
+        public Creature(Habitat habitat, int xpos, int ypos, int seed)
         {
             this.habitat = habitat;
             this.xpos = xpos;
             this.ypos = ypos;
-            this.gene = 0;
+            this.genes = new int[connections];
+            Random rand = new Random(seed);
+            for (int i = 0; i < connections; i++)
+            {
+                this.genes[i] = rand.Next(0, 268435455);
+                rand = new Random(seed+1);
+            }
         }
 
-        public Creature(Habitat habitat, int xpos, int ypos, int gene)
+        public Creature(Habitat habitat, int xpos, int ypos, int[] genes)
         {
             this.habitat = habitat;
             this.xpos = xpos;
             this.ypos = ypos;
-            this.gene = gene;
+            this.genes = genes;
         }
 
-        public int Gene => this.gene;
+        public int[] Genes => genes;
 
         private float SenseUp()
         {
@@ -78,9 +170,78 @@ namespace EvolutionSimulator
             }
         }
 
+        private float SenseAge() => habitat.Time/habitat.MaxTime;
+
+        private void MoveUp(int Signal)
+        {
+            habitat.SetPosition(xpos, ypos, null);
+            ypos -= 1;
+            habitat.SetPosition(xpos, ypos, this);
+        }
+
+        private void MoveDown(int Signal)
+        {
+            habitat.SetPosition(xpos, ypos, null);
+            ypos += 1;
+            habitat.SetPosition(xpos, ypos, this);
+        }
+
+        private void MoveRight(int Signal)
+        {
+            habitat.SetPosition(xpos, ypos, null);
+            xpos += 1;
+            habitat.SetPosition(xpos, ypos, this);
+        }
+
+        private void MoveLeft(int Signal)
+        {
+            habitat.SetPosition(xpos, ypos, null);
+            xpos -= 1;
+            habitat.SetPosition(xpos, ypos, this);
+        }
+
+        private float Input(int ID)
+        {
+            if (ID == 0)
+            {
+                return SenseUp();
+            }
+            else if (ID == 1)
+            {
+                return SenseDown();
+            }
+            else if (ID == 2)
+            {
+                return SenseRight();
+            }
+            else if (ID == 3)
+            {
+                return SenseLeft();
+            }
+            else if (ID == 4)
+            {
+                return SenseAge();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        private void Output(int[] Signals)
+        {
+            MoveUp(Signals[0]);
+            MoveDown(Signals[1]);
+            MoveRight(Signals[2]);
+            MoveLeft(Signals[3]);
+        }
+
         public void Action()
         {
-
+            for (int i = 0; i < connections; i++)
+            {
+                
+            }
         }
     }
 
@@ -89,6 +250,7 @@ namespace EvolutionSimulator
         private List<Creature> creatures = new List<Creature>();
         private int generation;
         private int maxTime;
+        private int time;
         private int mutationChance;
         private int reproduceChance;
         private int creatureAmount;
@@ -135,7 +297,7 @@ namespace EvolutionSimulator
                     if (xpos >= width) { xpos = 0; }
                     if (ypos >= length) { ypos = 0; }
                 }
-                creatures.Add(new Creature(this, xpos, ypos));
+                creatures.Add(new Creature(this, xpos, ypos, Modify()));
                 positions[ypos,xpos] = creatures[i];
             }
         }
@@ -152,23 +314,23 @@ namespace EvolutionSimulator
             return true;
         }
 
-        public Creature GetCreature(int index)
+        public Creature GetCreature(int index) => creatures[index];
+
+        public Creature GetPosition(int x, int y) => positions[y, x];
+
+        public void SetPosition(int x, int y, Creature creature)
         {
-            return creatures[index];
+            positions[y, x] = creature;
         }
 
-        public Creature GetPosition(int x, int y)
-        {
-            return positions[y,x];
-        }
+        public int Generation => generation;
 
-        public int GetGeneration()
-        {
-            return generation;
-        }
+        public int Time => time;
 
-        private int Mutate(int gene) {
-            int newGene = gene;
+        public int MaxTime => maxTime;
+
+        private int[] Mutate(int[] genes) {
+            int[] newGene = genes;
             return newGene;
         }
 
@@ -196,7 +358,7 @@ namespace EvolutionSimulator
                        if (xpos >= width) { xpos = 0; }
                        if (ypos >= length) { ypos = 0; }
                    }
-                   creatures.Add(new Creature(this, xpos, ypos, Mutate(parents[i%parents.Count].Gene)));
+                   creatures.Add(new Creature(this, xpos, ypos, Mutate(parents[i%parents.Count].Genes)));
                    positions[ypos,xpos] = creatures[i];
                 }
             }
@@ -209,6 +371,7 @@ namespace EvolutionSimulator
                 for (int j = 0; j < creatures.Count; j++)
                 {
                     creatures[j].Action();
+                    time = j;
                 }
             }
 
@@ -232,7 +395,7 @@ namespace EvolutionSimulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine(0xFFFFFFF);
         }
     }
 }
